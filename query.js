11e7716c9fba09800225c998813660ce66856789
Mick Wang <himrking@gmail.com>
2018-12-01 00:20:08 +0800

    add SQL to get game features by store

--- query.js
+++ query.js
@@ -67,8 +67,17 @@ app.get('/name/:keyword', (req, res, next) => {
 });
 
 // 查詢某個場館下的遊戲，欄位包含遊戲ID、遊戲名稱、價格、最低人數、最高人數、圖片檔名
+// app.get('/store/:sid', (req, res, next) => {
+//     connection.query("SELECT p.`PRO_SEQ`, p.`PRO_NAME`, p.`PRICE`, p.`PEOPLE_MIN`, p.`PEOPLE_MAX`, p_img.`IMG_NAME` FROM `products` p JOIN `product_images` p_img ON p.`PRO_SEQ`=p_img.`PRO_SEQ` WHERE p_img.`STATUS`='M' AND P_ID=?", req.params.sid, (err, results)=>{
+//         if(err){return res.send(err)};
+//         return res.json({
+//             data:results
+//         })
+//     });
+// });
+// 查詢某個場館下的遊戲，欄位包含遊戲ID、遊戲名稱、價格、最低人數、最高人數、圖片檔名、評價x3
 app.get('/store/:sid', (req, res, next) => {
-    connection.query("SELECT p.`PRO_SEQ`, p.`PRO_NAME`, p.`PRICE`, p.`PEOPLE_MIN`, p.`PEOPLE_MAX`, p_img.`IMG_NAME` FROM `products` p JOIN `product_images` p_img ON p.`PRO_SEQ`=p_img.`PRO_SEQ` WHERE p_img.`STATUS`='M' AND P_ID=?", req.params.sid, (err, results)=>{
+    connection.query("SELECT p.`PRO_NAME`, p.`PRICE`, p.`PEOPLE_MIN`, p.`PEOPLE_MAX`, p_img.`IMG_NAME`, f1.`feature` 'F1', c1.`CATE_NAME` 'FEATURE1', f2.`feature` 'F2', c2.`CATE_NAME` 'FEATURE2', f3.`feature` 'F3', c3.`CATE_NAME` 'FEATURE3' FROM `products` p LEFT JOIN `product_images` p_img ON p.`PRO_SEQ`=p_img.`PRO_SEQ` LEFT JOIN `gamefeature` f1 ON p.`PRO_SEQ` = f1.`gid` LEFT JOIN `game_cate` c1 ON f1.`feature` = c1.`sid` LEFT JOIN `gamefeature` f2 ON p.`PRO_SEQ` = f2.`gid` && f2.`feature` != f1.`feature` LEFT JOIN `game_cate` c2 ON f2.`feature` = c2.`sid` LEFT JOIN `gamefeature` f3 ON p.`PRO_SEQ` = f2.`gid` && f3.`feature` != f1.`feature` && f3.`feature` != f2.`feature` LEFT JOIN `game_cate` c3 ON f3.`feature` = c3.`sid` WHERE p_img.`STATUS`='M' AND p.`P_ID`=? GROUP BY p.`PRO_NAME`", req.params.sid, (err, results)=>{
         if(err){return res.send(err)};
         return res.json({
             data:results
