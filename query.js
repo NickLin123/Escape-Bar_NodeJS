18cc732a49e70fdef01efbd4124569a7be9f7261
mick <himrking@gmail.com>
2018-12-03 18:08:14 +0800

    article get API

--- query.js
+++ query.js
@@ -67,8 +67,18 @@ app.get('/name/:keyword', (req, res, next) => {
 });
 
 // 查詢某個場館下的遊戲，欄位包含遊戲ID、遊戲名稱、價格、最低人數、最高人數、圖片檔名
+// app.get('/store/:sid', (req, res, next) => {
+//     connection.query("SELECT p.`PRO_SEQ`, p.`PRO_NAME`, p.`PRICE`, p.`PEOPLE_MIN`, p.`PEOPLE_MAX`, p_img.`IMG_NAME` FROM `products` p JOIN `product_images` p_img ON p.`PRO_SEQ`=p_img.`PRO_SEQ` WHERE p_img.`STATUS`='M' AND P_ID=?", req.params.sid, (err, results)=>{
+//         if(err){return res.send(err)};
+//         return res.json({
+//             data:results
+//         })
+//     });
+// });
+
+// 查詢某個場館下的遊戲，欄位包含遊戲ID、遊戲名稱、價格、最低人數、最高人數、圖片檔名、特色x3
 app.get('/store/:sid', (req, res, next) => {
-    connection.query("SELECT p.`PRO_SEQ`, p.`PRO_NAME`, p.`PRICE`, p.`PEOPLE_MIN`, p.`PEOPLE_MAX`, p_img.`IMG_NAME` FROM `products` p JOIN `product_images` p_img ON p.`PRO_SEQ`=p_img.`PRO_SEQ` WHERE p_img.`STATUS`='M' AND P_ID=?", req.params.sid, (err, results)=>{
+    connection.query("SELECT p.`PRO_NAME`, p.`PRICE`, p.`PEOPLE_MIN`, p.`PEOPLE_MAX`, p_img.`IMG_NAME`, f1.`feature` F1, c1.`CATE_NAME` Feature1, f2.`feature` F2, c2.`CATE_NAME` Feature2, f3.`feature` F3, c3.`CATE_NAME` Feature3 FROM `products` p LEFT JOIN `product_images` p_img ON p.`PRO_SEQ`=p_img.`PRO_SEQ` LEFT JOIN `gamefeature` f1 ON p.`PRO_SEQ` = f1.`gid` LEFT JOIN `game_cate` c1 ON f1.`feature` = c1.`sid` LEFT JOIN `gamefeature` f2 ON p.`PRO_SEQ` = f2.`gid` && f2.`feature` != f1.`feature` LEFT JOIN `game_cate` c2 ON f2.`feature` = c2.`sid` LEFT JOIN `gamefeature` f3 ON p.`PRO_SEQ` = f2.`gid` && f3.`feature` != f1.`feature` && f3.`feature` != f2.`feature` LEFT JOIN `game_cate` c3 ON f3.`feature` = c3.`sid` WHERE p_img.`STATUS`='M' AND p.`P_ID`=? GROUP BY p.`PRO_NAME`", req.params.sid, (err, results)=>{
         if(err){return res.send(err)};
         return res.json({
             data:results
@@ -77,7 +87,15 @@ app.get('/store/:sid', (req, res, next) => {
 });
 
 
-
+// 部落格文章
+app.get('/article/:name', (req, res, next) => {
+    connection.query("SELECT * FROM articles WHERE article_name=?", req.params.name, (err, results)=>{
+        if(err){return res.send(err)};
+        return res.json({
+            data:results
+        })
+    });
+});
 
 app.listen(4000, ()=>
     console.log(`Server is listening on port 4000`)
--- test.sql
+++ test.sql
@@ -16,3 +16,4 @@ WHERE p_img.`STATUS`='M' AND p.`PRO_SEQ`=3 LIMIT 1
 WHERE p_img.`STATUS`='M' AND p.`P_ID`=21 LIMIT 1
 
 
+SELECT p.`PRO_NAME`, p.`PRICE`, p.`PEOPLE_MIN`, p.`PEOPLE_MAX`, p_img.`IMG_NAME`, f1.`feature` "F1", c1.`CATE_NAME` "FF1", f2.`feature` "F2", c2.`CATE_NAME` "FF2", f3.`feature` "F3", c3.`CATE_NAME` "FF3" FROM `products` p LEFT JOIN `product_images` p_img ON p.`PRO_SEQ`=p_img.`PRO_SEQ` LEFT JOIN `gamefeature` f1 ON p.`PRO_SEQ` = f1.`gid` LEFT JOIN `game_cate` c1 ON f1.`feature` = c1.`sid` LEFT JOIN `gamefeature` f2 ON p.`PRO_SEQ` = f2.`gid` && f2.`feature` != f1.`feature` LEFT JOIN `game_cate` c2 ON f2.`feature` = c2.`sid` LEFT JOIN `gamefeature` f3 ON p.`PRO_SEQ` = f2.`gid` && f3.`feature` != f1.`feature` && f3.`feature` != f2.`feature` LEFT JOIN `game_cate` c3 ON f3.`feature` = c3.`sid` WHERE p_img.`STATUS`='M' AND p.`P_ID`=21 GROUP BY p.`PRO_NAME`
